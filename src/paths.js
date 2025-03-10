801c322de8030428db5e45cdddc300f0f8e5957d
valentinochiappanni <valentino.chiappanni@estudiantes.unahur.edu.ar>
2025-03-10 13:26:21 -0300

    Page

---
+++ src/paths.js
@@ -0,0 +1,5 @@
+export const PATH = '/'
+export const PATH_CURSADA = `/cursada`
+export const PATH_CONTRATO_PEDAGOGICO = `/contrato-pedagogico`
+export const PATH_VIDEOS = `/videos`
+export const PATH_MATERIAL = `/material`
